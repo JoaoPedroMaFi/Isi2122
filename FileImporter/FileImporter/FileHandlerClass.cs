ebc056172dabb0330363dde2ce190f8087909c1f
AndreFelicidade <64292068+AndreFelicidade@users.noreply.github.com>
2021-11-17 11:55:37 +0000

    XML uploader reads xml properly

--- FileImporter/FileImporter/FileHandlerClass.cs
+++ FileImporter/FileImporter/FileHandlerClass.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System.Xml.XPath;
 
 namespace FileImporter
 {
@@ -22,6 +23,10 @@ namespace FileImporter
             //MessageBox.Show("File Content at path: " + filePath);
         }
 
+        /// <summary>
+        /// determines selected file extension by looking at the end of the path
+        /// </summary>
+        /// <param name="fp"></param>
         public static void FileReader(string fp)
         {
             string ext;
@@ -37,6 +42,7 @@ namespace FileImporter
             else if (ext == ".xml")
             {
                 MessageBox.Show("entrei no xml");
+                xmlUploader(fp);
             }
             else
             {
@@ -47,7 +53,39 @@ namespace FileImporter
         }
         //debug
         //MessageBox.Show("File Content at path: " + fp);
-    
 
+        /// <summary>
+        /// uploads xml files
+        /// sources: https://docs.microsoft.com/en-us/dotnet/api/system.io.stream?view=net-5.0
+        ///          https://docs.microsoft.com/en-us/dotnet/api/system.xml.xpath.xpathdocument?view=net-5.0
+        /// </summary>
+        /// <param name="filepath"></param>
+        public static void xmlUploader(string filepath)
+        {
+            XPathDocument doc;
+            XPathNavigator nav;
+            XPathNodeIterator node;
+            string title;
+
+            //using (FileStream stream = File.Open(filepath, FileMode.Open))
+           
+            doc = new(filepath);
+            nav = doc.CreateNavigator();
+            title = "Read file";
+            node = nav.Select("/");
+            ShowNode(title,node);
+           
+
+        }
+
+        static void ShowNode(string title, XPathNodeIterator node)
+        {
+            // title
+            MessageBox.Show($"{title}\n  count:{ node.Count }");
+
+            // show results
+            while (node.MoveNext())
+                MessageBox.Show(node.Current.OuterXml);
+        }
     }
 }
---
+++ FileImporter/FileImporter/Models/Isolado.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileImporter.Models
+{
+    public class Isolado
+    {
+        /// <summary>
+        /// id of the inspectioned, infected person
+        /// </summary>
+        public int idisolado { get; set; }
+
+        /// <summary>
+        /// id of the team inspecting that person
+        /// </summary>
+        public int idequipa { get; set; }
+
+        /// <summary>
+        /// first name of the inspectioned person
+        /// </summary>
+        public string firstname { get; set; }
+
+        /// <summary>
+        /// last name of the inspectioned person
+        /// </summary>
+        public string lastname { get; set; }
+
+        /// <summary>
+        /// Date of the inspection
+        /// </summary>
+        public string data { get; set; }
+
+        /// <summary>
+        /// boolean dictating if the person was isolating or not
+        /// true for yes, false for no
+        /// </summary>
+        public bool respeitou { get; set; }
+
+
+    }
+}
