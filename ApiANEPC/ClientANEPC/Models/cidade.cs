093f8ac6dc724d7d3b1e9a9cbd157332c6cfd488
AndreFelicidade <64292068+AndreFelicidade@users.noreply.github.com>
2021-11-22 13:36:24 +0000

    did client models

---
+++ ApiANEPC/ClientANEPC/Models/cidade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientANEPC.Models
+{
+    public partial class cidade
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public cidade()
+        {
+            this.equipas = new HashSet<equipa>();
+        }
+
+        public int idcidade { get; set; }
+        public string cidade_nome { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<equipa> equipas { get; set; }
+    }
+}
---
+++ ApiANEPC/ClientANEPC/Models/encomenda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientANEPC.Models
+{
+    public partial class encomenda
+    {
+        public int idencomenda { get; set; }
+        public int idequipa { get; set; }
+        public sbyte entregue { get; set; }
+
+        public virtual equipa equipa { get; set; }
+    }
+}
