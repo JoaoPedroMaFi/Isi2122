882876e64157f043c21325fefd11b2dfe0e51b59
AndreFelicidade <64292068+AndreFelicidade@users.noreply.github.com>
2021-11-23 14:56:47 +0000

    get all cities working

--- NewApiANEPC/NewApiANEPC/Controllers/CidadeController.cs
+++ NewApiANEPC/NewApiANEPC/Controllers/CidadeController.cs
@@ -12,13 +12,17 @@ namespace NewApiANEPC.Controllers
     [ApiController]
     public class CidadeController : Controller
     {
-
+        /// <summary>
+        /// Returns all cities in the database
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("getAll")]
         public async Task<ActionResult<IEnumerable<Cidade>>> GetAllCidadesAsync()
         {
             string sqlConStr = "Server=localhost;user=root;password=1234;Database=mydb_isi";
             using var connection = new MySqlConnection(sqlConStr);
+            //open connection
             await connection.OpenAsync();
 
             string query = @"
@@ -27,14 +31,26 @@ namespace NewApiANEPC.Controllers
             using var command = new MySqlCommand(query, connection);
             using var reader = await command.ExecuteReaderAsync();
 
+            List<string> list = new();
+            string val2;
             while (await reader.ReadAsync())
             {
-                var value = reader.GetValue(0);
-                return Ok(value);
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    var value = reader.GetValue(i);
+                    val2 = value.ToString();
+                    list.Add(val2);
+                }
             }
+            
+
+            //Close the reader
+            await reader.CloseAsync();
+            //Close connection
+            await connection.CloseAsync();
 
 
-            return Ok();
+            return Ok(list);
         }
     }
 }
