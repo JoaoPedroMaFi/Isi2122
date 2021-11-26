68e4fefc89dabee29321cfd9b49c164d9eb78097
AndreFelicidade <64292068+AndreFelicidade@users.noreply.github.com>
2021-11-26 15:22:18 +0000

    IT WORKS i mean consulta de encomendas a funcionar, tabela das encomendas apenas

--- NewApiANEPC/ClientANEPC/PainelEncomendasForm.Designer.cs
+++ NewApiANEPC/ClientANEPC/PainelEncomendasForm.Designer.cs
@@ -58,6 +58,7 @@ namespace ClientANEPC
             this.listView1.Size = new System.Drawing.Size(234, 195);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // PainelEncomendasForm
